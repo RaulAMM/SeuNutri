d7668aa1b9fd56946ae5e1d8b0b6982410afa6c6
Raul Augusto <raul.martins@aluno.ifsp.edu.br>
2026-09-23 11:27:11 -0300

    Primeiros passos

---
+++ src/main/java/edu/ifsp/seunutri/SeunutriApplication.java
@@ -0,0 +1,13 @@
+package edu.ifsp.seunutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeunutriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeunutriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ifsp/seunutri/SeunutriApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifsp.seunutri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeunutriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
